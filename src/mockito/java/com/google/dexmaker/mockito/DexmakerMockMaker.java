5a7820356e68a977711afc854d6cd71296c56391
Jesse Wilson <jessewilson@google.com>
2012-01-30 16:54:41 -0500

    Native mockito support. This depends on a few things not yet available in Mockito's HEAD.

---
+++ src/mockito/java/com/google/dexmaker/mockito/DexmakerMockMaker.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.dexmaker.mockito;
+
+import com.google.dexmaker.stock.ProxyBuilder;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.plugins.MockMaker;
+import org.mockito.plugins.MockSettingsInfo;
+import org.mockito.plugins.MockitoInvocationHandler;
+
+/**
+ * Generates mock instances on Android's runtime.
+ */
+public final class DexmakerMockMaker implements MockMaker {
+    private final UnsafeAllocator unsafeAllocator = UnsafeAllocator.create();
+
+    public <T> T createMock(Class<T> typeToMock, Class<?>[] extraInterfaces,
+            MockitoInvocationHandler handler, MockSettingsInfo settings) {
+        // TODO
+        System.setProperty("dexmaker.dexcache", "/data/vogar"); // TODO
+        // TODO
+        
+        InvocationHandler invocationHandler = new InvocationHandlerAdapter(handler);
+
+        if (typeToMock.isInterface()) {
+            // support interfaces via java.lang.reflect.Proxy
+            Class[] classesToMock = new Class[extraInterfaces.length + 1];
+            classesToMock[0] = typeToMock;
+            System.arraycopy(extraInterfaces, 0, classesToMock, 1, extraInterfaces.length);
+            @SuppressWarnings("unchecked") // newProxyInstance returns the type of typeToMock
+            T mock = (T) Proxy.newProxyInstance(typeToMock.getClassLoader(),
+                    classesToMock, invocationHandler);
+            return mock;
+
+        } else {
+            // support concrete classes via dexmaker's ProxyBuilder
+            try {
+                Class<? extends T> proxyClass = ProxyBuilder.forClass(typeToMock)
+                        .implementing(extraInterfaces)
+                        .buildProxyClass();
+                T mock = unsafeAllocator.newInstance(proxyClass);
+                Field handlerField = proxyClass.getDeclaredField("$__handler");
+                handlerField.setAccessible(true);
+                handlerField.set(mock, invocationHandler);
+                return mock;
+            } catch (RuntimeException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new MockitoException("Failed to mock " + typeToMock, e);
+            }
+        }
+    }
+
+    public void resetMock(Object mock, MockitoInvocationHandler newHandler, MockSettingsInfo settings) {
+        InvocationHandlerAdapter adapter = getInvocationHandlerAdapter(mock);
+        adapter.setHandler(newHandler);
+    }
+
+    public MockitoInvocationHandler getHandler(Object mock) {
+        InvocationHandlerAdapter adapter = getInvocationHandlerAdapter(mock);
+        return adapter != null ? adapter.getHandler() : null;
+    }
+    
+    private InvocationHandlerAdapter getInvocationHandlerAdapter(Object mock) {
+        if (Proxy.isProxyClass(mock.getClass())) {
+            InvocationHandler invocationHandler = Proxy.getInvocationHandler(mock);
+            return invocationHandler instanceof InvocationHandlerAdapter
+                    ? (InvocationHandlerAdapter) invocationHandler
+                    : null;
+        }
+
+        if (ProxyBuilder.isProxyClass(mock.getClass())) {
+            InvocationHandler invocationHandler = ProxyBuilder.getInvocationHandler(mock);
+            return invocationHandler instanceof InvocationHandlerAdapter
+                    ? (InvocationHandlerAdapter) invocationHandler
+                    : null;
+        }
+
+        return null;
+    }
+}
---
+++ src/mockito/java/com/google/dexmaker/mockito/InvocationHandlerAdapter.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.dexmaker.mockito;
+
+import com.google.dexmaker.stock.ProxyBuilder;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import org.mockito.internal.invocation.Invocation;
+import org.mockito.internal.invocation.MockitoMethod;
+import org.mockito.internal.invocation.realmethod.RealMethod;
+import org.mockito.internal.progress.SequenceNumber;
+import org.mockito.internal.util.ObjectMethodsGuru;
+import org.mockito.plugins.MockitoInvocationHandler;
+
+/**
+ * Handles proxy method invocations to dexmaker's InvocationHandler by calling
+ * a MockitoInvocationHandler.
+ */
+final class InvocationHandlerAdapter implements InvocationHandler {
+    private MockitoInvocationHandler handler;
+    private final ObjectMethodsGuru objectMethodsGuru = new ObjectMethodsGuru();
+
+    public InvocationHandlerAdapter(MockitoInvocationHandler handler) {
+        this.handler = handler;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (objectMethodsGuru.isEqualsMethod(method)) {
+            return proxy == args[0];
+        } else if (objectMethodsGuru.isHashCodeMethod(method)) {
+            return System.identityHashCode(proxy);
+        }
+
+        if (args == null) {
+            throw new IllegalArgumentException();
+        }
+
+
+        ProxiedMethod proxiedMethod = new ProxiedMethod(method);
+        return handler.handle(new Invocation(proxy, proxiedMethod, args, SequenceNumber.next(),
+                proxiedMethod));
+    }
+
+    public MockitoInvocationHandler getHandler() {
+        return handler;
+    }
+
+    public void setHandler(MockitoInvocationHandler handler) {
+        this.handler = handler;
+    }
+
+    private static class ProxiedMethod implements MockitoMethod, RealMethod {
+        private final Method method;
+
+        public ProxiedMethod(Method method) {
+            this.method = method;
+        }
+
+        public String getName() {
+            return method.getName();
+        }
+
+        public Class<?> getReturnType() {
+            return method.getReturnType();
+        }
+
+        public Class<?>[] getParameterTypes() {
+            return method.getParameterTypes();
+        }
+
+        public Class<?>[] getExceptionTypes() {
+            return method.getExceptionTypes();
+        }
+
+        public boolean isVarArgs() {
+            return method.isVarArgs();
+        }
+
+        public Method getJavaMethod() {
+            return method;
+        }
+
+        public Object invoke(Object target, Object[] arguments) throws Throwable {
+            return ProxyBuilder.callSuper(target, method, arguments);
+        }
+    }
+}

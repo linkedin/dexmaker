32e6c7982fb7650c42a7341056ea28d4011205c5
Tom Wilson <tom.wilson@blueapron.com>
2016-10-06 18:35:29 -0400

    Undo import re-order

--- mockito/src/main/java/com/android/dx/mockito/InvocationHandlerAdapter.java
+++ mockito/src/main/java/com/android/dx/mockito/InvocationHandlerAdapter.java
@@ -17,16 +17,15 @@
 package com.android.dx.mockito;
 
 import com.android.dx.stock.ProxyBuilder;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import org.mockito.internal.debugging.LocationImpl;
 import org.mockito.internal.invocation.InvocationImpl;
 import org.mockito.internal.invocation.MockitoMethod;
 import org.mockito.internal.invocation.realmethod.RealMethod;
 import org.mockito.internal.progress.SequenceNumber;
 import org.mockito.invocation.MockHandler;
-
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 /**
  * Handles proxy method invocations to dexmaker's InvocationHandler by calling

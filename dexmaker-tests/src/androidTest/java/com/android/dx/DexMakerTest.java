1bceaf0ad2993ff7e234329ac5e779c1c17fad0e
vmutafov <vlado.mutafov@gmail.com>
2019-07-18 22:32:51 +0300

    Add support for abstract and native methods generation

--- dexmaker-tests/src/androidTest/java/com/android/dx/DexMakerTest.java
+++ dexmaker-tests/src/androidTest/java/com/android/dx/DexMakerTest.java
@@ -481,6 +481,57 @@ public final class DexMakerTest {
     }
 
     @Test
+    public void testDeclareNativeMethod() throws Exception {
+        /*
+         * class Generated {
+         *   public Generated() {
+         *   }
+         *   public native void nativeMethod();
+         * }
+         */
+
+        addDefaultConstructor();
+        String nativeMethodName = "nativeMethod";
+        MethodId<?, Void> nativeMethodToGenerate = GENERATED.getMethod(TypeId.VOID, nativeMethodName);
+        dexMaker.declare(nativeMethodToGenerate, java.lang.reflect.Modifier.PUBLIC | java.lang.reflect.Modifier.NATIVE);
+
+        Class<?> generatedClass = generateAndLoad();
+        Object instance = generatedClass.getConstructor().newInstance();
+        Method nativeMethod = instance.getClass().getMethod(nativeMethodName);
+
+        assertTrue((nativeMethod.getModifiers() & NATIVE) != 0);
+        assertTrue((nativeMethod.getModifiers() & PUBLIC) != 0);
+        assertEquals(void.class, nativeMethod.getReturnType());
+        assertEquals(nativeMethodName, nativeMethod.getName());
+        assertEquals(nativeMethod.getParameterTypes().length, 0);
+    }
+
+    @Test
+    public void testDeclareAbstractClassWithAbstractMethod() throws Exception {
+        /*
+         * public abstract class AbstractClass {
+         *   public abstract void abstractMethod();
+         * }
+         */
+
+        dexMaker.declare(GENERATED, "AbstractClass.java", PUBLIC, TypeId.OBJECT);
+
+        String abstractMethodName = "abstractMethod";
+        MethodId<?, Void> nativeMethodToGenerate = GENERATED.getMethod(TypeId.VOID, abstractMethodName);
+        dexMaker.declare(nativeMethodToGenerate, java.lang.reflect.Modifier.PUBLIC | ABSTRACT);
+
+        Class<?> generatedClass = generateAndLoad();
+        Method nativeMethod = generatedClass.getMethod(abstractMethodName);
+
+        assertTrue((nativeMethod.getModifiers() & ABSTRACT) != 0);
+        assertTrue((nativeMethod.getModifiers() & PUBLIC) != 0);
+        assertEquals(void.class, nativeMethod.getReturnType());
+        assertEquals(abstractMethodName, nativeMethod.getName());
+        assertEquals(nativeMethod.getParameterTypes().length, 0);
+
+    }
+
+    @Test
     public void testReturnType() throws Exception {
         testReturnType(boolean.class, true);
         testReturnType(byte.class, (byte) 5);
@@ -1956,26 +2007,6 @@ public final class DexMakerTest {
     }
 
     @Test
-    public void testAbstractMethodsAreUnsupported() {
-        MethodId<?, Void> methodId = GENERATED.getMethod(TypeId.VOID, "call");
-        try {
-            dexMaker.declare(methodId, ABSTRACT);
-            fail();
-        } catch (IllegalArgumentException expected) {
-        }
-    }
-
-    @Test
-    public void testNativeMethodsAreUnsupported() {
-        MethodId<?, Void> methodId = GENERATED.getMethod(TypeId.VOID, "call");
-        try {
-            dexMaker.declare(methodId, NATIVE);
-            fail();
-        } catch (IllegalArgumentException expected) {
-        }
-    }
-
-    @Test
     public void testSynchronizedFieldsAreUnsupported() {
         try {
             FieldId<?, ?> fieldId = GENERATED.getField(TypeId.OBJECT, "synchronizedField");
@@ -2005,7 +2036,6 @@ public final class DexMakerTest {
     // TODO: don't generate multiple times (?)
     // TODO: test array types
     // TODO: test generating an interface
-    // TODO: declare native method or abstract method
     // TODO: get a thrown exception 'e' into a local
     // TODO: move a primitive or reference
 
